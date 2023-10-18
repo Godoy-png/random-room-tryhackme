bceda2969ecab776e40658a5738662205aa4e056
Gabriel Godoy <107765540+Godoy-png@users.noreply.github.com>
2023-10-18 17:11:58 -0300

    Update random-room.py

--- random-room.py
+++ random-room.py
@@ -2,9 +2,7 @@ import requests
 import random
 from colorama import Fore, Style, init
 
-
 init(autoreset=True)
-
 
 def get_user_choice(prompt, valid_choices):
     while True:
@@ -13,7 +11,6 @@ def get_user_choice(prompt, valid_choices):
             return user_input
         else:
             print(Fore.RED + "Please respond with 'Y' for Yes or 'N' for No." + Style.RESET_ALL)
-
 
 def get_difficulty_choice():
     while True:
@@ -32,7 +29,6 @@ def get_difficulty_choice():
         else:
             print(Fore.RED + "Please choose a valid option (0-5)." + Style.RESET_ALL)
 
-
 difficulty_options = {
     0: "all",
     1: "info",
@@ -48,40 +44,39 @@ api_url = "https://tryhackme.com/api/hacktivities?page=1&order=newest&type=all&l
 user_choice_free = get_user_choice("Do you want " + Fore.GREEN + "only free" + Style.RESET_ALL + " rooms? [" + Fore.GREEN + "Y" + Style.RESET_ALL + " / " + Fore.GREEN + "N" + Style.RESET_ALL + "]: ", ["Y", "N"])
 
 if user_choice_free == "Y":
-
     api_url += "&free=free"
 
 user_choice_difficulty = get_difficulty_choice()
 chosen_difficulty = difficulty_options[user_choice_difficulty]
 
-response = requests.get(api_url)
+while True:
+    response = requests.get(api_url)
 
-if response.status_code == 200:
-    data = response.json()
+    if response.status_code == 200:
+        data = response.json()
 
-    # Filtrar salas com "userCompleted" igual a False
-    rooms = [room for room in data["rooms"] if not room["userCompleted"]]
+        rooms = [room for room in data["rooms"] if not room["userCompleted"]]
 
-    if chosen_difficulty != "all":
-        rooms = [room for room in rooms if room["difficulty"] == chosen_difficulty]
+        if chosen_difficulty != "all":
+            rooms = [room for room in rooms if room["difficulty"] == chosen_difficulty]
 
-    available_rooms = {room["code"]: room for room in rooms}
+        available_rooms = {room["code"]: room for room in rooms}
 
-    if available_rooms:
-        random_room_code = random.choice(list(available_rooms.keys()))
-        random_room = available_rooms[random_room_code]
-        room_difficulty = random_room["difficulty"]
+        if available_rooms:
+            random_room_code = random.choice(list(available_rooms.keys()))
+            random_room = available_rooms[random_room_code]
+            room_difficulty = random_room["difficulty"]
 
-        print("\nRandomly chosen room: " + Fore.GREEN + random_room['title'] + Style.RESET_ALL)
-        print("Room difficulty: " + Fore.GREEN + room_difficulty + Style.RESET_ALL)
-        print("Room link: https://tryhackme.com/room/" + random_room_code)
+            print("\nRandomly chosen room: " + Fore.GREEN + random_room['title'] + Style.RESET_ALL)
+            print("Room difficulty: " + Fore.GREEN + room_difficulty + Style.RESET_ALL)
+            print("Room link: https://tryhackme.com/room/" + random_room_code)
 
+            play_again = get_user_choice("\n\nDo you want to draw again? [" + Fore.GREEN + "Y " + Style.RESET_ALL +"/" + Fore.RED + " N" + Style.RESET_ALL + "]: ", ["Y", "N"])
+            if play_again == "N":
+                break
+        else:
+            print(Fore.RED + "You have completed all available rooms or no matching rooms are available." + Style.RESET_ALL)
+            break
     else:
-        print(Fore.RED + "You have completed all available rooms or no matching rooms are available." + Style.RESET_ALL)
-else:
-    print(Fore.RED + "Request to TryHackMe API failed." + Style.RESET_ALL)
+        print(Fore.RED + "Request to TryHackMe API failed." + Style.RESET_ALL)
 
-
-exit_command = input(Fore.RED + "\n \nEnter 'EXIT' to exit: " + Style.RESET_ALL)
-if exit_command.strip().upper() == "EXIT":
-    exit()
